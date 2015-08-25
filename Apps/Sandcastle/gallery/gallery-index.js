f2457e050374f46b0d3423a56534ae3a119aab12
Xiyao <will2247@hotmail.com>
2015-08-25 21:15:09 +0200

    Initial commit

---
+++ Apps/Sandcastle/gallery/gallery-index.js
@@ -0,0 +1,146 @@
+// This file is automatically rebuilt by the Cesium build process.
+var gallery_demos = [{
+  "name": "3D Models",
+  "date": 1438264406469,
+  "img": "3D Models.jpg"
+}, {
+  "name": "ArcGIS MapServer",
+  "date": 1435078979584,
+  "img": "ArcGIS MapServer.jpg"
+}, {
+  "name": "Billboards",
+  "date": 1434984183740,
+  "img": "Billboards.jpg"
+}, {
+  "name": "Box",
+  "date": 1434984183755,
+  "img": "Box.jpg"
+}, {
+  "name": "CZML",
+  "date": 1434984183755,
+  "img": "CZML.jpg"
+}, {
+  "name": "Camera Tutorial",
+  "date": 1434984183755,
+  "img": "Camera Tutorial.jpg"
+}, {
+  "name": "Camera",
+  "date": 1438264406506,
+  "img": "Camera.jpg"
+}, {
+  "name": "Cesium Inspector",
+  "date": 1434984183771,
+  "img": "Cesium Inspector.jpg"
+}, {
+  "name": "Cesium Widget",
+  "date": 1434984183771,
+  "img": "Cesium Widget.jpg"
+}, {
+  "name": "Circles and Ellipses",
+  "date": 1434984183786,
+  "img": "Circles and Ellipses.jpg"
+}, {
+  "name": "Corridor",
+  "date": 1434984183786,
+  "img": "Corridor.jpg"
+}, {
+  "name": "Custom DataSource",
+  "date": 1434984183786,
+  "img": "Custom DataSource.jpg"
+}, {
+  "name": "Cylinders and Cones",
+  "date": 1434984183786,
+  "img": "Cylinders and Cones.jpg"
+}, {
+  "name": "GeoJSON and TopoJSON",
+  "date": 1434984183802,
+  "img": "GeoJSON and TopoJSON.jpg"
+}, {
+  "name": "GeoJSON simplestyle",
+  "date": 1434984183802,
+  "img": "GeoJSON simplestyle.jpg"
+}, {
+  "name": "Geometry and Appearances",
+  "date": 1434984183802,
+  "img": "Geometry and Appearances.jpg"
+}, {
+  "name": "Hello World",
+  "date": 1434984183818,
+  "img": "Hello World.jpg"
+}, {
+  "name": "Imagery Adjustment",
+  "date": 1434984183818,
+  "img": "Imagery Adjustment.jpg"
+}, {
+  "name": "Imagery Layers Manipulation",
+  "date": 1434984183818,
+  "img": "Imagery Layers Manipulation.jpg"
+}, {
+  "name": "Imagery Layers",
+  "date": 1434984183833,
+  "img": "Imagery Layers.jpg"
+}, {
+  "name": "Interpolation",
+  "date": 1438264346623,
+  "img": "Interpolation.jpg"
+}, {
+  "name": "KML",
+  "date": 1434984183833,
+  "img": "KML.jpg"
+}, {
+  "name": "Labels",
+  "date": 1434984183833,
+  "img": "Labels.jpg"
+}, {
+  "name": "Map Pins",
+  "date": 1434984183849,
+  "img": "Map Pins.jpg"
+}, {
+  "name": "Materials",
+  "date": 1434984183849,
+  "img": "Materials.jpg"
+}, {
+  "name": "Picking",
+  "date": 1436192243754,
+  "img": "Picking.jpg"
+}, {
+  "name": "Points",
+  "date": 1434984183864,
+  "img": "Points.jpg"
+}, {
+  "name": "Polygon",
+  "date": 1434984183864,
+  "img": "Polygon.jpg"
+}, {
+  "name": "Polyline Volume",
+  "date": 1434984183864,
+  "img": "Polyline Volume.jpg"
+}, {
+  "name": "Polyline",
+  "date": 1434984183880,
+  "img": "Polyline.jpg"
+}, {
+  "name": "Rectangle",
+  "date": 1434984183880,
+  "img": "Rectangle.jpg"
+}, {
+  "name": "Show or Hide Entities",
+  "date": 1434984183880,
+  "img": "Show or Hide Entities.jpg"
+}, {
+  "name": "Spheres and Ellipsoids",
+  "date": 1434984183880,
+  "img": "Spheres and Ellipsoids.jpg"
+}, {
+  "name": "Terrain",
+  "date": 1434984183896,
+  "img": "Terrain.jpg"
+}, {
+  "name": "Wall",
+  "date": 1434984183896,
+  "img": "Wall.jpg"
+}, {
+  "name": "Web Map Service (WMS)",
+  "date": 1435078979584,
+  "img": "Web Map Service (WMS).jpg"
+}];
